d709ec6c0654547b63b8e141f84ed378bd758774
alaa-obeid <alaa.obeid@enkrypt.com.au>
2023-10-24 02:51:03 +0300

    added standard security measures

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
+//packages
 require("express-async-errors");
 require("dotenv").config();
+
+//error handlers
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+
+//routes
+const authRoute = require("./routes/auth");
+const jobsRoute = require("./routes/jobs");
+
+//middlewares
+const authMiddleware = require("./middleware/authentication");
+
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const ratelimiter = require("express-rate-limit");
+
 const express = require("express");
 const connectToDatabase = require("./db/connect");
 
 const app = express();
 
-const errorHandlerMiddleware = require("./middleware/error-handler");
-const notFoundMiddleware = require("./middleware/not-found");
-const authRoute = require("./routes/auth");
-const jobsRoute = require("./routes/jobs");
-const authMiddleware = require("./middleware/authentication");
-
+app.set("trust proxy", 1);
+app.use(ratelimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/jobs", authMiddleware, jobsRoute);
